79ed0361201aae183b13dd8324d053a8468f38bb
Vitalii-Saienko <vitaliy5saie@gmail.com>
2023-02-05 13:29:59 +0100

    initial commit

---
+++ src/Level8/FindNearestSquareNumber.java
@@ -0,0 +1,19 @@
+package Level8;
+
+public class FindNearestSquareNumber {
+    /*
+    https://www.codewars.com/kata/5a805d8cafa10f8b930005ba
+    Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), of a positive integer n.
+    For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square
+    of 11, than 100, the square of 10.
+    If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+     */
+    public static void main(String[] args) {
+        System.out.println(nearestSq(1));
+        System.out.println(nearestSq(2));
+        System.out.println(nearestSq(111));
+    }
+    public static int nearestSq(final int n){
+        return (int) Math.pow(Math.round(Math.sqrt(n)),2);
+    }
+}
